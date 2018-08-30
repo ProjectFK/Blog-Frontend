8a53f305910c5eac7f7aabb3d43dbd04e3663e50
Mike Lu <heatmint2333@gmail.com>
2018-08-30 21:42:02 +0800

    Publish page

---
+++ src/publish/app.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+var xss = require("xss");
+
+document.getElementById("publishbut").onclick = function (){
+	var title = xss(document.getElementById('blogtitle').value);
+	if(title == ""){
+		alert("Please enter the blogtitle!");
+	}
+	else{
+		var html = xss(editor.txt.html());
+		console.log(title);
+		console.log(html);
+	}
+}
