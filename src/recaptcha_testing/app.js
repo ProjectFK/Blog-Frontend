51c372dc0f02c2bbdf50a39aeff100a91faaf024
wusatosi <wusatosi@sina.com>
2018-08-31 19:54:51 +0800

    Implement recaptcha using API

--- src/recaptcha_testing/app.js
+++ src/recaptcha_testing/app.js
@@ -1,1 +1,12 @@
-//Nothing
+let siteKey = '6LepWGkUAAAAAOuDkXsDYx5ohu-kas5-As7x047v';
+
+window.onload = () => {
+    grecaptcha.render('button', {
+        'sitekey': siteKey,
+        'callback': onRecaptchaTokenObtained
+    });
+};
+
+var onRecaptchaTokenObtained = (token) => {
+    alert(token);
+};
