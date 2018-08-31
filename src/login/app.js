418838df8958530a17684d43990d8307d3a3beef
Mike Lu <heatmint2333@gmail.com>
2018-08-31 18:20:22 +0800

    Using recaptcha(still have issue)
    Using recaptcha instead of vaptcha, haing issue of binding function

--- src/login/app.js
+++ src/login/app.js
@@ -1,21 +1,14 @@
 import './style.css';
 
-const HmacSHA1 = require("crypto-js/hmac-sha1");
+var loginbut = document.getElementById("login");
 
-function getTimeStamp() {
-    return Math.round(new Date().getTime() / 1000);
-}
+loginbut.setAttribute('data-callback','login');
 
-var fakevap = "12345678";//a fake vaptcha challenge id,used before vaptch online
-
-document.getElementById("login").onclick = function () {
-    const username = document.getElementById("username").value;
+function login(captcha){
+	const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    var hash = HmacSHA1(username + password + getTimeStamp(), fakevap);
-    var stringified = hash.toString();
-    //ready to fetch here
-    console.log(hash);
-    console.log(stringified);
+    console.log(username + "," + password)
+    console.log(captcha)
     //log fetch() information to be ready for next step and debug
 };
 
