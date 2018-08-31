5fa1e6f79c6800e85631cd70b6e92c812b4fb670
wusatosi <wusatosi@sina.com>
2018-08-31 19:54:51 +0800

    Implement recaptcha using API

--- src/login/app.js
+++ src/login/app.js
@@ -7,10 +7,10 @@ loginbut.setAttribute('data-callback','login');
 function login(captcha){
 	const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    console.log(username + "," + password)
+    console.log(username + "," + password);
     console.log(captcha)
     //log fetch() information to be ready for next step and debug
-};
+}
 
 console.log('app.js loaded!');
 
